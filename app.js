18abb1cc8aac74e13607aa6603b4c3ef15b26453
Yun Ji <this.jiyun@gmail.com>
2022-08-24 10:25:19 +0200

    Aligned card border color with Pokemon color

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const searchInput = document.querySelector("[data-search]")
 searchInput.addEventListener("input", (e) => {
     const value = e.target.value
@@ -8,8 +9,7 @@ searchInput.addEventListener("input", (e) => {
 const pokemonCardTemplate = document.querySelector("[data-pokemon-template]")
 const pokemonCardContainer = document.querySelector("[data-pokemon-cards-container]")
 
-
-
+// Fetch all the pokemons
 fetch("https://pokeapi.co/api/v2/pokemon")
     .then(res => res.json())
     .then(data => {
@@ -17,6 +17,7 @@ fetch("https://pokeapi.co/api/v2/pokemon")
         console.log(data.results)
         const divRow = document.createElement("div")
         divRow.classList.add("row")
+        // For each Pokemon, create a card with Bootstrap classed divs
         data.results.forEach(pokemon => {
             const divCol = document.createElement("div")
             divCol.classList.add("col-12", "col-lg-4", "pb-4")
@@ -26,7 +27,7 @@ fetch("https://pokeapi.co/api/v2/pokemon")
             const image = card.querySelector("[data-pokemon-image]")
             const name = card.querySelector("[data-pokemon-name]")
             name.textContent = pokemon.name
-
+            // for each Pokemon, get the pokemon img and the color 
             fetch(`${pokemon.url}`)
                 .then(res => res.json())
                 .then(data => {
@@ -34,13 +35,20 @@ fetch("https://pokeapi.co/api/v2/pokemon")
                     id.textContent = `#${data.id}`
                     image.src = data.sprites.front_default
 
+                    //Fetch pokemon color by id
+                    fetch(`https://pokeapi.co/api/v2/pokemon-species/${data.id}`)
+                        .then(res => res.json())
+                        .then(data => {
+                            console.log(data.color.name)
+                            color = data.color.name
+                            card.style.borderColor = color
+                        })
+
                 })
 
             divCol.append(card)
             divRow.append(divCol)
             pokemonCardContainer.append(divRow)
-
-
         })
 
 
